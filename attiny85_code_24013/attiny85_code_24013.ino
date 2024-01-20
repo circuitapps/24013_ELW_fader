7901605d3b0fb7c0ec276161bf430405ad30eabd
circuitapps <125308597+circuitapps@users.noreply.github.com>
2024-01-19 18:24:53 -0800

    Bench tested and verified code
    Fading effect timings have been carefully selected and tested for the best visual experience. This version does not have any known bugs.

--- attiny85_code_24013/attiny85_code_24013.ino
+++ attiny85_code_24013/attiny85_code_24013.ino
@@ -1,10 +1,12 @@
 
 /*
-  Fading effect using an EL Wire and a microcontroller
+  Fading effect using an EL Wire and AtTiny85 microcontroller
 
   Operation: This code is designed to control an EL wire such that it displays a fade in and fade out light effect. This is not possible using the original inverter EL wires typically come with.
   We use ATtiny85 microcontroller to perform the timing and the frequency of the ON/OFF pulse patterns generated for achieving the fading effect. Timing has been carefully measured via bench tests
   and has been optimized to provide the best visual experience.
+
+  You can play with the values in pwm_levels[] array and its length as well as the LED_EFFECT_WAIT_TIME definition to experiment with different fading effects. Sky is the limit here!
   
   by circuitapps
   January 2024
@@ -22,78 +24,68 @@
 // Application pin definitions
 #define ELW_OUT_PIN PB2_PIN7
 
-#define LOOP_1_LIMIT 100
+#define PWM_OUT_PIN PB1_PIN6
+#define PWM_REGISTER OCR1A  // This is where the high duration (out of a max of 255) needs to be written
 
-void turn_on(unsigned int pin_name)
+#define FADE_OUT 0
+#define FADE_IN 1
+#define LED_EFFECT_WAIT_TIME 175  // Every LED_EFFECT_WAIT_TIME ms a new LED light intensity will be observed
+
+
+void timer_counter_1_pwm_setup()
 {
-  digitalWrite(pin_name, HIGH);
+  TCCR1 = 1<<PWM1A | 1<<COM1A0 | 1<<CS10;
+  // OC1A is the output to pin 6 of Attiny85 (as per datasheet)
+  pinMode(PWM_OUT_PIN, OUTPUT);  // pwm is programmed to be output here
 }
-
-void turn_off(unsigned int pin_name)
-{
-  digitalWrite(pin_name, LOW);
-}
-
-void wait_ms(unsigned int delay_ms)
-{
-  delay(delay_ms);
-}
-
-// EL WIRE ILLUMINATION PATTERN 1:
-void (*pattern_1_funcs[])(unsigned int) = {&turn_on,          &wait_ms,    &turn_off,   &wait_ms};
-//unsigned int pattern_1_func_args[] =      {ELW_OUT_PIN,       10,         ELW_OUT_PIN,  10};
-
-unsigned int *pattern_1_func_args[] = {
-                                          (unsigned int[]){ELW_OUT_PIN,       20,         ELW_OUT_PIN,  20},
-                                          (unsigned int[]){ELW_OUT_PIN,       30,         ELW_OUT_PIN,  30},
-                                          (unsigned int[]){ELW_OUT_PIN,       35,         ELW_OUT_PIN,  35},
-                                          (unsigned int[]){ELW_OUT_PIN,       40,         ELW_OUT_PIN,  40},
-                                          (unsigned int[]){ELW_OUT_PIN,       45,         ELW_OUT_PIN,  45},
-                                          (unsigned int[]){ELW_OUT_PIN,       50,         ELW_OUT_PIN,  50},
-                                          (unsigned int[]){ELW_OUT_PIN,       70,         ELW_OUT_PIN,  70}
-                                        };
-
-  int number_of_loops[] = {25, 17, 15, 13, 11, 10, 7};  // NEEDS TO HAVE THE SAME NUMBER OF ELEMENTS AS THE NUMBER OF ROWS IN *pattern_1_func_args[]
 
 // the setup function runs once when you press reset or power the board
-void setup() {
-  pinMode(ELW_OUT_PIN, OUTPUT);
-
-  digitalWrite(ELW_OUT_PIN, LOW);
+void setup()
+{
+  timer_counter_1_pwm_setup();  // Sets PWM_OUT_PIN as PWM output
 }
 
 // the loop function runs over and over again forever
 
-void loop() {
+void loop()
+{
+  unsigned int count_state = FADE_IN;  // start with fade in effect
+  //  Any maximum pwm_levels[] element can be 255. This determines the LED intensity level.
+  unsigned int pwm_levels[] = {0,1,2,3,4,5,15,100,150,200,255};  // carefully selected through experimentation for best visual fading effect
+  unsigned char current_level_idx = 0;
 
-  int i, loop_idx, arg_row;
-  int num_pattern_1_elements = sizeof(pattern_1_funcs) / sizeof(pattern_1_funcs[0]);
-  int num_arg_rows = sizeof(pattern_1_func_args) / sizeof(pattern_1_func_args[0]);
+  unsigned int max_len = sizeof(pwm_levels) / sizeof(pwm_levels[0]);
 
-  // SCAN FORWARD
-  for( arg_row = 0; arg_row < num_arg_rows; ++arg_row )
+  while(1)
   {
-    for(loop_idx = 0 ; loop_idx < number_of_loops[arg_row]; ++loop_idx)
-    {
-      for(i = 0 ; i < num_pattern_1_elements; ++i)
-      {// Cycling through each pattern defined by the user.
-        pattern_1_funcs[i](pattern_1_func_args[arg_row][i]);  // calling the function via function pointers and the specified function argument.
+    PWM_REGISTER = pwm_levels[current_level_idx];
+    delay(LED_EFFECT_WAIT_TIME);  // Wait time in ms before current LED light pattern is switched.
+
+    if(count_state == FADE_IN)
+    {// we are in FADE_IN phase
+      if(current_level_idx == (max_len - 1))
+      {// reached the FADE_IN limit
+        count_state = FADE_OUT;  // time to switch to FADE_OUT state
+        --current_level_idx;  // down one register value to kick off fade out next.
       }
-
-    }// end loop_idx
-  }// end arg_row
-
-  // SCAN BACKWARD
-  for( arg_row = num_arg_rows - 1; arg_row >=0 ; --arg_row )
-  {
-    for(loop_idx = 0 ; loop_idx < number_of_loops[arg_row]; ++loop_idx)
-    {
-      for(i = 0 ; i < num_pattern_1_elements; ++i)
-      {// Cycling through each pattern defined by the user.
-        pattern_1_funcs[i](pattern_1_func_args[arg_row][i]);  // calling the function via function pointers and the specified function argument.
+      else
+      {// FADE_IN limit not yet reached
+        ++current_level_idx;  // up one register value for next fade in period.
       }
+    }
+    else
+    {// we are in the FADE_OUT phase
+      if(current_level_idx == 0)
+      {// reached the FADE_OUT lower limit
+        count_state = FADE_IN;  // time to switch to FADE_IN state
+        ++current_level_idx;  // up one register value to kick off fade in next.
+      }
+      else
+      {// FADE_OUT lower limit not yet reached
+        --current_level_idx;  // down one register value for next fade out period.
+      }
+    }
+  }
 
-    }// end loop_idx
-  }// end arg_row
-
+  //fader_state_machine();
 }
